5780b4791379824db67d60d17f714ab27c944299
Daniel Olivas <Daniel.Olivas27@hotmail.com>
2022-06-05 11:15:11 -0600

    (perf): Se cambio la estructura de los archivos php y se agrego informacion en acercade.php

--- inicarSesion.php
+++ inicarSesion.php
@@ -1,36 +1,12 @@
-<!DOCTYPE html>
-<html lang="es">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>registro</title>
+<?php  
 
-    <link rel="stylesheet" href="../style/style.css" />
+$pagina = isset($_GET ['p']) ? strtolower($_GET ['p']) : 'index';
 
-  </head>
-  <body>
-    <header class="header">
-      <nav class="nav">
-        <a href="index.html" class="logo nav-link">PackXpress</a>
-        <ul class="nav-menu">
-          <li class="nav-menu-item">  <a href="rastreo.html" class="nav-menu-link nav-link">Rastreo</a>
-          </li>
-          <li class="nav-menu-item">  <a href="acercade.html" class="nav-menu-link nav-link">Acerca de</a>
-          </li>
-          <li class="nav-menu-item"> <a href="servicioalcliente.html" class="nav-menu-link nav-link" >servicio al cliente</a>
-          </li>
-          <li class="nav-menu-item"> <a href="inicarSecion.html" class="nav-menu-link nav-link " style="color: black" >Inicia sesión</a>
-          </li>
-          <li class="nav-menu-item">  <a href="registro.php" class="nav-menu-link nav-link">Registrarse</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+require_once 'general/header.php';
 
 
-   
-     
+?>
+
     <section class="form-register2">
       <h5>Ingrese su correo y constresña para iniciar sesión</h5>
       <input class="controls" type="email" name="correo" id="email" placeholder="Ingrese su Correo">
@@ -43,8 +19,3 @@
     </section>
 
 
-    <footer>
-      <l>Copiright &copy; 2022 - PackXpress - All Right Reserved </l>
-  </footer> 
-  </body>
-</html>
